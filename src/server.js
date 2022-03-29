b1adf028b5ab51dfc569479ebadbf0f28d94ac5c
Pierre Rudolph <p.r@gamebeast.de>
2022-03-29 22:41:02 +0200

    Cannot read property 'latitude' of undefined fixed Output: Debug added via env:debug "true" as string

--- src/server.js
+++ src/server.js
@@ -14,7 +14,6 @@ var options = {
   username: "" + process.env.mqtt_username,
   password: "" + process.env.mqtt_password,
   clean: false,
-  debug: false,
   reconnectPeriod: 1 * 1000,
 };
 
@@ -49,7 +48,9 @@ console.log(`Running on http://${HOST}:${PORT}`);
 // working on POST DATA from traccar
 app.post('/', function (request, response) {
   console.log("post incoming");
-  //console.log(request.body); // PRINT json data from Traccar (carefully with privacy!)
+  if (process.env.debug == "true") {
+    console.log(request.body); // PRINT json data from Traccar (carefully with privacy!)
+  }
   send2mqtt(request.body);
   response.send("");
 });
@@ -57,32 +58,34 @@ app.post('/', function (request, response) {
 //  setup and send to mqtt broker
 function send2mqtt(json) {
 
-  if (mqtt_client.connected == true) {
-    let cache = {};
-    if (typeof json.device.name != "undefined") { cache["name"] = json.device.name.replace(" ", "_"); }
-    if (typeof json.device.status != "undefined") { cache["status"] = json.device.status; }
-    if (typeof json.device.geofenceIds != "undefined") { cache["geofenceIds"] = json.device.geofenceIds.toString(); }
-    if (typeof json.device.lastUpdate != "undefined") { cache["lastUpdate"] = json.device.lastUpdate; }
-    if (typeof json.device.model != "undefined") { cache["model"] = json.device.model; }
-    if (typeof json.device.uniqueid != "undefined") { cache["uniqueid"] = json.device.uniqueid; }
-    if (typeof json.device.positionid != "undefined") { cache["positionid"] = json.device.positionid; }
-    if (typeof json.position.latitude != "undefined") { cache["lat"] = json.position.latitude; }
-    if (typeof json.position.longitude != "undefined") { cache["lon"] = json.position.longitude; }
-    if (typeof json.position.altitude != "undefined") { cache["altitude"] = json.position.altitude; }
-    if (typeof json.position.course != "undefined") { cache["course"] = json.position.course; }
-    if (typeof json.position.speed != "undefined") { cache["speed"] = json.position.speed; }
-    if (typeof json.position.accuracy != "undefined") { cache["accuracy"] = json.position.accuracy; }
-    if (typeof json.position.network != "undefined") { cache["network"] = json.position.network; }
-    if (typeof json.position.deviceTime != "undefined") { cache["deviceTime"] = json.position.deviceTime; }
-    if (typeof json.position.address != "undefined") { cache["address"] = json.position.address; }
-    if (typeof json.position.attributes.BATTERY != "undefined") { cache["BATTERY"] = json.position.attributes.BATTERY; }
-    if (typeof json.position.attributes.distance != "undefined") { cache["distance"] = json.position.attributes.distance; }
-    if (typeof json.position.attributes.totalDistance != "undefined") { cache["totalDistance"] = json.position.attributes.totalDistance; }
-    if (typeof json.position.attributes.motion != "undefined") { cache["motion"] = json.position.attributes.motion; }
-    if (typeof json.position.attributes.batteryLevel != "undefined") { cache["batteryLevel"] = json.position.attributes.batteryLevel; }
-    for (var key in cache) {
-      var value = '' + cache[key]
-      mqtt_client.publish('traccar/' + cache["name"] + "/" + key, value, { retain: true, qos: 1 });
-    };
+  if (mqtt_client.connected == true) {  // mqtt connected?
+    if (typeof json.position.protocol != "undefined") { // is it a device?
+      let cache = {};
+      if (typeof json.device.name != "undefined") { cache["name"] = json.device.name.replace(" ", "_"); }
+      if (typeof json.device.status != "undefined") { cache["status"] = json.device.status; }
+      if (typeof json.device.geofenceIds != "undefined") { cache["geofenceIds"] = json.device.geofenceIds.toString(); }
+      if (typeof json.device.lastUpdate != "undefined") { cache["lastUpdate"] = json.device.lastUpdate; }
+      if (typeof json.device.model != "undefined") { cache["model"] = json.device.model; }
+      if (typeof json.device.uniqueid != "undefined") { cache["uniqueid"] = json.device.uniqueid; }
+      if (typeof json.device.positionid != "undefined") { cache["positionid"] = json.device.positionid; }
+      if (typeof json.position.latitude != "undefined") { cache["lat"] = json.position.latitude; }
+      if (typeof json.position.longitude != "undefined") { cache["lon"] = json.position.longitude; }
+      if (typeof json.position.altitude != "undefined") { cache["altitude"] = json.position.altitude; }
+      if (typeof json.position.course != "undefined") { cache["course"] = json.position.course; }
+      if (typeof json.position.speed != "undefined") { cache["speed"] = json.position.speed; }
+      if (typeof json.position.accuracy != "undefined") { cache["accuracy"] = json.position.accuracy; }
+      if (typeof json.position.network != "undefined") { cache["network"] = json.position.network; }
+      if (typeof json.position.deviceTime != "undefined") { cache["deviceTime"] = json.position.deviceTime; }
+      if (typeof json.position.address != "undefined") { cache["address"] = json.position.address; }
+      if (typeof json.position.attributes.BATTERY != "undefined") { cache["BATTERY"] = json.position.attributes.BATTERY; }
+      if (typeof json.position.attributes.distance != "undefined") { cache["distance"] = json.position.attributes.distance; }
+      if (typeof json.position.attributes.totalDistance != "undefined") { cache["totalDistance"] = json.position.attributes.totalDistance; }
+      if (typeof json.position.attributes.motion != "undefined") { cache["motion"] = json.position.attributes.motion; }
+      if (typeof json.position.attributes.batteryLevel != "undefined") { cache["batteryLevel"] = json.position.attributes.batteryLevel; }
+      for (var key in cache) {
+        var value = '' + cache[key]
+        mqtt_client.publish('traccar/' + cache["name"] + "/" + key, value, { retain: true, qos: 1 });
+      };
+    }
   }
 };
